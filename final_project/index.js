03def53adc512c351547b0a07bf29cc356b5ab81
renoNG2023 <122050927+renoNG2023@users.noreply.github.com>
2023-11-03 12:49:36 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,15 +8,43 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// JWT secret key (keep this secret)
+const JWT_SECRET = 'your-secret-key';
+
+// Custom middleware for user authentication
+app.use("/customer/auth/*", function auth(req, res, next) {
+  // Extract the token from the request headers or query parameters
+  const token = req.headers.authorization || req.query.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized. Token missing.' });
+  }
+
+  // Verify the JWT token
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized. Invalid token.' });
+    }
+
+    // User is authenticated, store user information for future routes
+    req.user = decoded;
+
+    next(); // Continue to the next middleware or route
+  });
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
