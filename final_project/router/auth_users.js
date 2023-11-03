2630b69732e96ba7258f3cc9548edcab287f70bc
renoNG2023 <122050927+renoNG2023@users.noreply.github.com>
2023-11-03 12:41:25 +0100

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,10 +7,27 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+  // Check if the username is valid
+  if (!isValid(username)) {
+    return res.status(400).json({ message: "Invalid username" });
+  }
+
+
+  
+
+  // If username and password are valid, create a JWT token
+  const token = jwt.sign({ username }, 'your-secret-key');
+
+  res.json({ token });
+});
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  if (!authenticatedUser(username, password)) {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
 }
 
 //only registered users can login
